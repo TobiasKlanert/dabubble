02f6bcd7e28b4b7816228938e4a75a8daf2a8e31
TobiasKlanert <tobias.klanert@t-online.de>
2025-07-10 22:39:00 +0200

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent }
+    { path: '', component: LoginComponent },
+    { path: 'main', component: MainComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
