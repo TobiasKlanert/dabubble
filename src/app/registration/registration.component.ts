8b0e6a5c3355f018a432b8d8d7f73f01f8e59ff2
Jan Steiner <jansteiner73@gmail.com>
2025-07-11 14:07:34 +0200

    registration design

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
