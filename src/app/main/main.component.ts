c3ab6c89f65259796deab16a1d358a544e1141b3
TobiasKlanert <tobias.klanert@t-online.de>
2025-07-11 23:08:21 +0200

    feat: added devspace, chat and threads to main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DevspaceComponent } from './devspace/devspace.component';
+import { ChatComponent } from './chat/chat.component';
+import { ThreadsComponent } from './threads/threads.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [DevspaceComponent, ChatComponent, ThreadsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
