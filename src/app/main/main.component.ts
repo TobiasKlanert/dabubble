70eaa094e67a37d59623635e2dad1fb92d5d4bd2
TobiasKlanert <tobias.klanert@t-online.de>
2025-07-11 22:08:32 +0200

    style: added header on main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
-
+  onSearch(value: any, inputRef?: HTMLInputElement, event?: Event): void {
+    if (event) {
+      event.preventDefault();
+    }
+    console.log('Search event:', value);
+    if (inputRef) {
+      inputRef.value = '';
+    }
+  }
 }
