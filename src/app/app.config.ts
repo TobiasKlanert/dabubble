3c01702b31e971ab6af6fc57894206ef904579e5
TobiasKlanert <tobias.klanert@t-online.de>
2025-07-12 10:32:02 +0200

    feat: added angular firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,25 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: '',
+        appId: '',
+        storageBucket: '',
+        apiKey: '',
+        authDomain: '',
+        messagingSenderId: '',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
