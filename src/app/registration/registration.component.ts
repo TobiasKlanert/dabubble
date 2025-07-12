a59bf6e6939a2fab935f5c029cc7b7ce13116747
Jan Steiner <jansteiner73@gmail.com>
2025-07-12 19:03:58 +0200

    style: registration form validation & genral form adjustments

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,17 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+ 
+  constructor(private fb: FormBuilder){}
+
+  form = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    privacy: [false, [Validators.requiredTrue]] 
+  })
 
 }
