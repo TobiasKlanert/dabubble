e35943d7e71ca2771cff76b692546b0b91f41e9a
Jan Steiner <jansteiner73@gmail.com>
2025-07-12 16:18:12 +0200

    style: login formvalidation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    ReactiveFormsModule,
+    RouterLink,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   showIntro: boolean = true;
+
+  constructor(private fb: FormBuilder){}
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
 
   ngOnInit(): void {
     setTimeout(() => {
