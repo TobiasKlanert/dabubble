519638cda5f304dc668a42af8c87e07380a07629
Jan Steiner <jansteiner73@gmail.com>
2025-07-16 20:13:47 +0200

    ts & style: choose avatar

--- src/app/choose-avatar/choose-avatar.component.ts
+++ src/app/choose-avatar/choose-avatar.component.ts
@@ -11,5 +11,23 @@ import { RouterLink } from '@angular/router';
   styleUrl: './choose-avatar.component.scss'
 })
 export class ChooseAvatarComponent {
+  selectedAvatar: string = '';
+
+  avatars: string[] = [
+    '/assets/img/user1.png',
+    '/assets/img/user2.png',
+    '/assets/img/user3.png',
+    '/assets/img/user4.png',
+    '/assets/img/user5.png',
+    '/assets/img/user6.png',
+  ]
+
+  selectAvatar(avatar: string): void {
+    this.selectedAvatar = avatar;
+  }
+
+  get isAvatarChosen(): boolean {
+    return this.selectedAvatar !== '';
+  }
 
 }
