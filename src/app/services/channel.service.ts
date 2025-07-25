115f83ec3299a06df86378cb25c47b29236a5db5
TobiasKlanert <tobias.klanert@t-online.de>
2025-07-25 21:49:36 +0200

    feat: added id and dummy data on channel model and service

--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Channel } from '../models/channel.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChannelService {
-  private channels: Channel[] = [];
+  private channels: Channel[] = [
+    {
+      id: '1',
+      name: 'Entwicklerteam',
+      description: '',
+    },
+    {
+      id: '2',
+      name: 'Office-Team',
+      description: '',
+    },
+  ];
   private channelsSubject = new BehaviorSubject<Channel[]>([]);
   channels$ = this.channelsSubject.asObservable();
+
+  constructor() {
+    this.channelsSubject.next(this.channels);
+  }
 
   addChannel(channel: Channel) {
     this.channels.push(channel);
     this.channelsSubject.next([...this.channels]);
+  }
+
+  createChannel(name: string, description?: string) {
+    const newChannel: Channel = {
+      id: uuidv4(),
+      name,
+      description,
+    };
+    this.addChannel(newChannel);
   }
 
   getChannels(): Channel[] {
