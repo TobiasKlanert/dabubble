524b14de2c0fe87cdfc3635abf228db3f3e952e6
Jan Steiner <jansteiner73@gmail.com>
2025-07-29 12:52:50 +0200

    refactor: style single-message & first reaction style

--- src/app/main/single-message/single-message.component.ts
+++ src/app/main/single-message/single-message.component.ts
@@ -1,25 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ProfileService } from '../../shared/services/profile.service';
+import { EmojiMenuComponent } from '../emoji-menu/emoji-menu.component';
+import { EmojiService } from '../../shared/services/emoji.service';
 
 
 @Component({
   selector: 'app-single-message',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    EmojiMenuComponent,
   ],
   templateUrl: './single-message.component.html',
   styleUrl: './single-message.component.scss'
 })
+
 export class SingleMessageComponent {
   @Input() text!: string;
   @Input() outgoing = false;
   @Input() timestamp?: string;
 
-  constructor(
-    private profileService: ProfileService
-  ) {}
+  constructor (private profileService: ProfileService) {}
 
   openProfile(userId: string) {
     this.profileService.openUserProfile(userId);
