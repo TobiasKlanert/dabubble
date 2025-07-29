dc95475c8c2c39186ba0f8ae1ce2f16b6dcde8a7
TobiasKlanert <tobias.klanert@t-online.de>
2025-07-29 22:13:41 +0200

    style: added html and style on channel-info component

--- src/app/main/overlay/channel-info/channel-info.component.ts
+++ src/app/main/overlay/channel-info/channel-info.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { OverlayService } from '../../../shared/services/overlay.service';
 
 @Component({
   selector: 'app-channel-info',
   standalone: true,
   imports: [],
   templateUrl: './channel-info.component.html',
-  styleUrl: './channel-info.component.scss'
+  styleUrl: './channel-info.component.scss',
 })
 export class ChannelInfoComponent {
+  constructor(private overlayService: OverlayService) {}
 
+  closeOverlay() {
+    this.overlayService.close();
+  }
 }
