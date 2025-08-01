fb2b3db39c01058bc5040b1ad2d7f971020d83c0
TobiasKlanert <tobias.klanert@t-online.de>
2025-08-01 21:46:34 +0200

    feat: added editor on channel-info component

--- src/app/main/overlay/channel-info/channel-info.component.ts
+++ src/app/main/overlay/channel-info/channel-info.component.ts
@@ -1,17 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { OverlayService } from '../../../shared/services/overlay.service';
+import { TextareaResizeService } from '../../../shared/services/textarea-resize.service';
 
 @Component({
   selector: 'app-channel-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel-info.component.html',
   styleUrl: './channel-info.component.scss',
 })
 export class ChannelInfoComponent {
-  constructor(private overlayService: OverlayService) {}
+  isNameEditorActive = false;
+  isDescriptionEditorActive = false;
+
+  constructor(
+    private overlayService: OverlayService,
+    public textareaResizeService: TextareaResizeService
+  ) {}
 
   closeOverlay() {
     this.overlayService.close();
+  }
+
+  toggleEditor(editor: string) {
+    switch (editor) {
+      case 'name':
+        this.isNameEditorActive = !this.isNameEditorActive;
+        break;
+      case 'description':
+        this.isDescriptionEditorActive = !this.isDescriptionEditorActive;
+        break;
+    }
+  }
+
+  saveEdit(editor: string) {
+    switch (editor) {
+      case 'name':
+        this.toggleEditor('name');
+        this.saveDataToArray('name');
+        break;
+      case 'description':
+        this.toggleEditor('description');
+        this.saveDataToArray('description');
+        break;
+    }
+  }
+
+  saveDataToArray(editor: string) {
+    //Placeholder
+    console.log(editor);
   }
 }
