d6555127238d710acf8ff5bb5002c8b064156ccc
Jan Steiner <jansteiner73@gmail.com>
2025-08-20 11:06:17 +0200

    feat: tread component frame

--- src/app/main/threads/threads.component.ts
+++ src/app/main/threads/threads.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { EmojiMenuComponent } from '../emoji-menu/emoji-menu.component';
+import { EmojiService } from '../../shared/services/emoji.service';
+import {
+  OverlayMenuType,
+  OverlayService,
+} from '../../shared/services/overlay.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-threads',
   standalone: true,
-  imports: [],
+  imports: [
+    EmojiMenuComponent,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './threads.component.html',
   styleUrl: './threads.component.scss'
 })
 export class ThreadsComponent {
 
-  activeMessage = [
+  threadMessage = [
     { text: 'Hey, wie geht’s?', outgoing: false, timestamp: '12:00' },
   ]
 
@@ -27,4 +39,53 @@ export class ThreadsComponent {
     { text: 'Gut und dir?', outgoing: true, timestamp: '12:01' },
     { text: 'Auch gut!', outgoing: false, timestamp: '12:02' },
   ]
+
+  inputText: string = '';
+
+  constructor(public emojiService: EmojiService, private overlayService: OverlayService,) { }
+
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    try {
+      const el = this.scrollContainer.nativeElement;
+      el.scrollTop = el.scrollHeight;
+    } catch (err) {
+      console.warn('Scroll to bottom failed:', err);
+    }
+  }
+
+  addTextEmoji = (emoji: string) => {
+    this.inputText += emoji;
+  };
+
+  toggleEmojiPicker() {
+    this.emojiService.toggleChannelPicker();
+  }
+
+  openOverlay(overlay: OverlayMenuType) {
+    this.overlayService.open(overlay);
+  }
+
+  sendMessage() {
+    if (this.inputText.trim()) {
+      const msg = {
+        text: this.inputText.trim(),
+        outgoing: true,
+        timestamp: new Date().toLocaleTimeString('de-DE', {
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+      };
+      console.log(msg);
+      this.messages.push(msg);
+      // this.messageService.addMessage(msg);
+      this.inputText = '';
+      setTimeout(() => this.scrollToBottom(), 0);
+    }
+  }
 }
