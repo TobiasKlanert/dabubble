b55cde76399ddb9a8ba486c69e629ae6d9b985c9
TobiasKlanert <tobias.klanert@t-online.de>
2025-09-10 20:51:15 +0200

    style: Style components added for editing the logged-in user

--- src/app/main/overlay/profile/profile.component.ts
+++ src/app/main/overlay/profile/profile.component.ts
@@ -22,6 +22,7 @@ export class ProfileComponent implements OnInit {
     onlineStatus: false,
   };
   isOwnProfile: boolean = true;
+  isEditModeActive: boolean = false;
 
   private destroy$ = new Subject<void>();
 
