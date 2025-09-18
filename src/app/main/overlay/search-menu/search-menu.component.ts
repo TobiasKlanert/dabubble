212d44a0f393ee85505148147df16142be9f91c8
TobiasKlanert <tobias.klanert@t-online.de>
2025-09-18 21:08:38 +0200

    feat: The Search Menu has been expanded to include the Search Type NewChat. This allows to search for a user and start a new chat with them.

--- src/app/main/overlay/search-menu/search-menu.component.ts
+++ src/app/main/overlay/search-menu/search-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { User } from '../../../shared/models/database.model';
-import { SearchType } from '../../../shared/models/chat.enums';
+import { ChatType, SearchType } from '../../../shared/models/chat.enums';
 import { OverlayService } from '../../../shared/services/overlay.service';
 import { FirestoreService } from '../../../shared/services/firestore.service';
+import { ChatService } from '../../../shared/services/chat.service';
 
 @Component({
   selector: 'app-search-menu',
@@ -16,12 +17,20 @@ export class SearchMenuComponent {
   @Input() searchResults: User[] = [];
   @Input() searchType!: SearchType;
 
+  loggedInUserId: string = '';
   selectedUsers: User[] = [];
 
   constructor(
     private overlayService: OverlayService,
-    private firestore: FirestoreService
+    private firestore: FirestoreService,
+    private chatService: ChatService
   ) {}
+
+  ngOnInit() {
+    this.firestore.loggedInUserId$.subscribe((id) => {
+      this.loggedInUserId = id;
+    })
+  }
 
   clickOnUser(id: string) {
     switch (this.searchType) {
@@ -33,6 +42,18 @@ export class SearchMenuComponent {
         const userToAdd = this.searchResults.find((user) => user.id === id);
         if (userToAdd && !this.selectedUsers.some((u) => u.id === id)) {
           this.selectedUsers.push(userToAdd);
+        }
+        break;
+      case SearchType.NewChat:
+        const user = this.searchResults.find(user => user.id === id);
+        
+        if (user) {
+          this.firestore.getOrCreateDirectChatId(this.loggedInUserId, user.id)
+            .then(chatId => {
+              this.chatService.selectChatId(chatId);
+              this.chatService.selectChatPartner(user);
+              this.chatService.selectChatType(ChatType.DirectMessage);
+            });
         }
         break;
       case SearchType.MentionUser:
--- src/app/shared/models/chat.enums.ts
+++ src/app/shared/models/chat.enums.ts
@@ -7,5 +7,6 @@ export enum ChatType {
 export enum SearchType {
   AddUser,
   ShowProfile,
-  MentionUser
+  MentionUser,
+  NewChat
 }
