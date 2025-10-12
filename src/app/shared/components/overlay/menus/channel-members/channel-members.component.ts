590586fd0dbd3abfbd9bc3849aa1813e1145ca69
TobiasKlanert <tobias.klanert@t-online.de>
2025-10-12 16:57:53 +0200

    style: implemented mobile view on channel info menu

--- src/app/shared/components/overlay/menus/channel-members/channel-members.component.ts
+++ src/app/shared/components/overlay/menus/channel-members/channel-members.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Subject, takeUntil, switchMap, tap } from 'rxjs';
 import { OverlayService } from '../../../../services/overlay.service';
 import { User } from '../../../../models/database.model';
 import { FirestoreService } from '../../../../services/firestore.service';
 import { ChatService } from '../../../../services/chat.service';
-import { ChatType } from '../../../../models/chat.enums';
+import { ScreenService } from '../../../../services/screen.service';
+import { ChatType, OverlayType } from '../../../../models/chat.enums';
 
 @Component({
   selector: 'app-channel-members',
@@ -15,25 +17,46 @@ import { ChatType } from '../../../../models/chat.enums';
 })
 export class ChannelMembersComponent {
   channelId: string = '';
+  overlayType: OverlayType = OverlayType.Normal;
+  OverlayType = OverlayType;
 
   channelMembers: User[] = [];
+
+  private destroy$ = new Subject<void>();
+
+  @Input() callFromChannelInfo: boolean = false;
 
   constructor(
     private overlayService: OverlayService,
     private firestore: FirestoreService,
     private chatService: ChatService,
+    private screenService: ScreenService
   ) {}
 
   ngOnInit() {
-    this.chatService.selectedChatId$.subscribe((chatId) => {
-      if (chatId) {
-        this.firestore
-          .getChannelMembers(chatId, ChatType.Channel)
-          .subscribe((members) => {
-            this.channelMembers = members;
-          });
-      }
-    })
+    // Kombiniere ChatId- und ScreenService-Subscription in einer Subscription
+    this.chatService.selectedChatId$
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap((chatId) => {
+          this.channelId = chatId;
+          if (chatId) {
+            return this.firestore.getChannelMembers(chatId, ChatType.Channel);
+          }
+          return [];
+        }),
+        tap((members) => (this.channelMembers = members)),
+        switchMap(() => this.screenService.isMobile$),
+        tap((isMobile) => {
+          this.overlayType = isMobile ? OverlayType.FullSize : OverlayType.Normal;
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   openProfile(userId: string) {
